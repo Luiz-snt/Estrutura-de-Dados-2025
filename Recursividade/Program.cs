a81fb0e8f8149b59a43aa24bda6cb02147e08df0
Luiz-snt <luizhenriqued076@gmail.com>
2025-08-20 20:14:06 -0300

    Add files via upload

---
+++ Recursividade/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Escreva um número para Finobacci séries:");
+int leght = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < leght; i++)
+{
+    Console.Write("{0}", FibonacciSeries(i));
+}
+Console.ReadKey();
+
+static int FibonacciSeries(int n);
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+}
