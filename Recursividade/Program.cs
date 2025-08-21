8d2d9bd231102bac175553f9f05e9fc89890f745
Luiz Henrique dos Santos <446984@unoesc.lan>
2025-08-20 21:35:05 -0300

    Recursividade

---
+++ Recursividade/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a number:");
+int leght = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < leght; i++)
+{
+    Console.Write("{0} ", fatorial(i));
+}
+Console.ReadKey();
+
+static int fatorial (int n)
+{
+    if (n == 0) return 1;
+    else return n * fatorial(n - 1);
+}
