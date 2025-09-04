c06689bfa5816cdbbbfdbebe80b5c4132254155f
Luiz Henrique dos Santos <446984@unoesc.lan>
2025-09-03 21:14:30 -0300

    Polindromo

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+Console.WriteLine("Invertendo palavras com pilhas (Stacks)");
+
+Stack<char> chars = new Stack<char>();
+string? palavra = Console.ReadLine();
+string? palavra_invertida = null;
+
+foreach (var c in palavra)
+
+    chars.Push(c);
+
+while (chars.Count > 0)
+{
+    palavra_invertida += chars.Pop();
+}
+if (palavra_invertida == palavra)
+{
+    Console.WriteLine("Essa Palavra é um Palíndromo");
+}
+else
+{
+    Console.WriteLine("Essa Palavra não é um Palíndromo");
+    Console.WriteLine(palavra_invertida);
+}
