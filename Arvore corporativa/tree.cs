1cad8f94364ea003927b5e704705b068e65cc5b7
Luiz Henrique dos Santos <446984@unoesc.lan>
2025-09-17 22:05:46 -0300

    Commit

---
+++ Arvore corporativa/tree.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arvore_corporativa
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+        
+    }
+}
