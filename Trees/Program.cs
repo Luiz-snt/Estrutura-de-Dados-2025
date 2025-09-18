2096bf2987e1dddf2e163b2dcf0f6df3a92e01eb
Luiz Henrique dos Santos <446984@unoesc.lan>
2025-09-17 21:36:43 -0300

    Tree

---
+++ Trees/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Trees;
+
+// Declaração de variável tree do tipo Tree, onde Tree<int> agora é de inteiros
+Tree<int> tree = new Tree<int>();
+
+// Root é o nó raiz da árvore, instanciado com seu tipo TreeNode, onde o TreeNode<int> por sua vez é inteiro
+tree.Root = new TreeNode<int>();
+tree.Root.Data = 100;
+
+tree.Root.Children = new List<TreeNode<int>>
+    {
+        new TreeNode<int>() { Data = 50, Parent = tree.Root },
+        new TreeNode<int>() { Data = 1, Parent = tree.Root },
+        new TreeNode<int>() { Data = 150, Parent = tree.Root },
+    };
+
+tree.Root.Children[2].Children = new List<TreeNode<int>>
+    {
+        new TreeNode<int>() { Data = 30, Parent = tree.Root.Children[2] },
+    };
