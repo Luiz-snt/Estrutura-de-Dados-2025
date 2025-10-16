75434075f0c6743faa5cc40dd459efb5b0d8fb5a
Luiz Henrique dos Santos <446984@unoesc.lan>
2025-10-15 21:50:42 -0300

    Commit

---
+++ Selection Sort/Program.cs
@@ -0,0 +1,14 @@
+﻿using Selection_Sort;
+Console.WriteLine("Vetor de inteiros:");
+
+int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+Selection_Sort.SelectionSort.Sort(integerValues);
+Console.WriteLine(string.Join(",", integerValues));
+Console.WriteLine("\n****************************\n");
+
+Console.WriteLine("Vetor de nomes:");
+
+string[] stringValues = { "Bolsonaro", "Kléber", "Putin", "Pica-Pau", "Barack Hussein Obama", "Tais Carla" };
+Selection_Sort.SelectionSort.Sort(stringValues);
+Console.WriteLine(string.Join(",", stringValues));
