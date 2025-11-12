be32edc1d5e374303c798707f3f5b9b449cacd6e
Luiz Henrique dos Santos <446984@unoesc.lan>
2025-11-12 20:37:18 -0300

    Algoritmo de Busca

---
+++ Algoritmo de Busca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class BuscaNoVetor
+{
+    public static void Main(string[] args)
+    {
+        List<string> list = ["10", "80", "62", "24", "37", "5", "30", "78", "91", "67"];
+
+        Console.WriteLine($"Dados: {string.Join(", ", list)}");
+        Console.Write("Digite um Número: ");
+        string? numero = Console.ReadLine();
+        bool encontrado = false;
+
+        if (numero != null)
+        {
+            for (int i = 0; i < list.Count; i++) 
+            {
+
+                if (numero.CompareTo(list[i]) == 0)
+                {
+                    Console.WriteLine($"✅ Número encontrado na posição: {i}");
+                    encontrado = true;
+                    break;
+                }
+            }
+        }
+
+        if (!encontrado)
+        {
+            Console.WriteLine("❌ Número não encontrado.");
+        }
+    }
+}
